795c07ce8fa7efd37244ba012fc9d3c894de02d3
AyresMonteiro <ayresmonteiro52@gmail.com>
2021-11-08 12:52:36 -0300

    create text domain in routes

---
+++ src/routes/text/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const folder = './src/routes/text/';
+const exported = {};
+const folderSynonyms = require('./synonyms');
+
+files = fs.readdirSync(folder);
+
+for (let i = 0; i < files.length; i++) {
+  const banned = ['.', '..', 'index.js', 'synonyms.js'];
+
+  if (banned.find((val) => val === files[i])) continue;
+
+  const [routeClass, synonyms] = require('./' + files[i]);
+
+  for (let j = 0; j < synonyms.length; j++) {
+    exported[synonyms[j]] = routeClass;
+  }
+}
+
+module.exports = [exported, folderSynonyms];
